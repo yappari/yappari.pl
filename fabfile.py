dc3be3e4a2027b8f2ae0200906782b0bf42b024a
kbielen <kzeslaf@gmail.com>
2013-01-04 00:48:30 +0100

    dodanie treści na stronie 'home'; dodanie pliku fabfile.py; dodanie 2 postów + dr. popr.

---
+++ fabfile.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+from fabric.api import local, put, sudo
+
+
+def prepare():
+    local('rm -rf _site')
+    local('jekyll')
+
+
+def deploy():
+    sudo('rm -rf /home/projects/_site')
+    put('_site', '/home/projects/', use_sudo=True)

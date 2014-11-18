f423332b11d57271633505409d5e98000ac0f458
kbielen <kzeslaf@gmail.com>
2014-11-18 23:14:17 +0100

    fabfile.py update

--- fabfile.py
+++ fabfile.py
@@ -6,10 +6,22 @@ from fabric.api import local, put, sudo
 
 
 def prepare():
+    """..."""
+    #
     local('rm -rf _site')
     local('jekyll build')
 
+    #
+    local('rm _site/README.md')
+    local('rm _site/fabfile.py')
+    local('rm _site/fabfile.pyc')
+
 
 def deploy():
+    """..."""
+    #
+    prepare()
+
+    #
     sudo('rm -rf /home/projects/_site')
     put('_site', '/home/projects/', use_sudo=True)
